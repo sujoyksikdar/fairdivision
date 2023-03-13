7bfa9485be3f87b3033deef998649315032546ad
sujoyksikdar <46714295+sujoyksikdar@users.noreply.github.com>
2023-03-13 16:09:51 -0400

    Fix bug with ispo() function, incorrect use of the chores flag
    Function ispo() replaced with code from the ispo2() function.
    
    Fixes an issue for goods only instances:
    Fixed a bug which caused the ILP within the function to be infeasible for goods only instances.
    The bug is due to incorrect use of the chores (True/False) input variable which is set to False for goods instances.
    This causes the upper bound for the utility of agents to be set to 0 while the lower bound is typically some positive integer.
    The upper bound is set to 0 because the desired upper bound is multiplied by the value of the chores variable setting it to 0. The upper bound should be set to the budget (or more precisely, the total value of all goods for the agent).
    
    The issue seems to have been introduced due to poor versioning earlier causing unfinished code to be committed.

--- property.py
+++ property.py
@@ -3,6 +3,16 @@
 # Output: True/False
 # is[eq/eq1/eqx/ef/ef1/efx/po](V, A)
 # WARNING: ispo ILP is sensitive to numerical issues when "budget" is large
+# IMPORTANT:
+#     We will assume that goods are assigned a value >= 0 in V
+#     and that chores are assigned a value <= 0 in V
+# NOTES:
+#     tested for goods only instances
+#     not qualified for chores only or mixed goods and chores instances
+#     for definitions for the case of goods only instances see Freeman et al. "Equitable Allocations of Indivisible Goods." In IJCAI 2019.
+#     for definitions for the case of chores only instances see Freeman et al. "Equitable Allocations of Indivisible Chores." In AAMAS 2020.
+# TODO:
+#     add chores functionality
 ###########################################################################
 
 import logging
@@ -13,19 +23,37 @@ import config
 
 
 def iseq(V, A, chores=False):
+    """
+    Input:
+        V: a valuation profile; an n x m 2D numpy array consisting of integers whose i.j-th entry V[i,j] represents agent i's the value for item j
+        A: an allocation; a binary n x m 2D numpy array consisting of 0s and 1s
+        chores: True / False indicating whether the input V represents a chores instance (True) or goods instance (False)
+    Output:
+        True / False: return True if A satisfies EQ w.r.t. the input valuation profile V
+    """
     (n, m) = np.shape(V)
-    U = np.zeros(n)
+    # compute the utility experienced by each agent from allocation A according to valuations in V
+    U = np.zeros(n) 
     for j in range(n):
         v = V[j,:]
         a = A[j,:]
         u = np.dot(v, a)
         U[j] = u
+    # check for equitability (EQ)
     if all(u == U[0] for u in U):
         return True
     return False
 
 
 def iseq1(V, A, chores=False):
+    """
+    Input:
+        V: a valuation profile; an n x m 2D numpy array consisting of integers whose i.j-th entry V[i,j] represents agent i's the value for item j
+        A: an allocation; a binary n x m 2D numpy array consisting of 0s and 1s
+        chores: True / False indicating whether the input V represents a chores instance (True) or goods instance (False)
+    Output:
+        True / False: return True if A satisfies equitability-up-to-one-item (EQ1) w.r.t. the input valuation profile V
+    """
     (n, m) = np.shape(V)
     for j1 in range(n):
         for j2 in range(n):
@@ -52,6 +80,14 @@ def iseq1(V, A, chores=False):
 
 
 def isdupeq1(V, A, chores=False):
+    """
+    Input:
+        V: a valuation profile; an n x m 2D numpy array consisting of integers whose i.j-th entry V[i,j] represents agent i's the value for item j
+        A: an allocation; a binary n x m 2D numpy array consisting of 0s and 1s
+        chores: True / False indicating whether the input V represents a chores instance (True) or goods instance (False)
+    Output:
+        True / False: return True if A is equitabile-up-to-one-duplicated-item (DEQ1) w.r.t. the input valuation profile V
+    """
     (n, m) = np.shape(V)
     for j1 in range(n):
         for j2 in range(n):
@@ -78,6 +114,14 @@ def isdupeq1(V, A, chores=False):
 
 
 def iseqx(V, A, chores=False):
+    """
+    Input:
+        V: a valuation profile; an n x m 2D numpy array consisting of integers whose i.j-th entry V[i,j] represents agent i's the value for item j
+        A: an allocation; a binary n x m 2D numpy array consisting of 0s and 1s
+        chores: True / False indicating whether the input V represents a chores instance (True) or goods instance (False)
+    Output:
+        True / False: return True if A is envy-free-up-to-any-item (EQX) w.r.t. the input valuation profile V
+    """
     (n, m) = np.shape(V)
     for j1 in range(n):
         for j2 in range(n):
@@ -114,6 +158,14 @@ def iseqx(V, A, chores=False):
 
 
 def isdupeqx(V, A, chores=False):
+    """
+    Input:
+        V: a valuation profile; an n x m 2D numpy array consisting of integers whose i.j-th entry V[i,j] represents agent i's the value for item j
+        A: an allocation; a binary n x m 2D numpy array consisting of 0s and 1s
+        chores: True / False indicating whether the input V represents a chores instance (True) or goods instance (False)
+    Output:
+        True / False: return True if A is equitabile-up-to-any-duplicated-item (DEQX) w.r.t. the input valuation profile V
+    """
     (n, m) = np.shape(V)
     for j1 in range(n):
         for j2 in range(n):
@@ -150,6 +202,14 @@ def isdupeqx(V, A, chores=False):
 
 
 def isef(V, A, chores=False):
+    """
+    Input:
+        V: a valuation profile; an n x m 2D numpy array consisting of integers whose i.j-th entry V[i,j] represents agent i's the value for item j
+        A: an allocation; a binary n x m 2D numpy array consisting of 0s and 1s
+        chores: True / False indicating whether the input V represents a chores instance (True) or goods instance (False)
+    Output:
+        True / False: return True if A is envy-free (EF) w.r.t. the input valuation profile V
+    """
     (n, m) = np.shape(V)
     for j1 in range(n):
         for j2 in range(n):
@@ -166,6 +226,14 @@ def isef(V, A, chores=False):
 
 
 def isef1(V, A, chores=False):
+    """
+    Input:
+        V: a valuation profile; an n x m 2D numpy array consisting of integers whose i.j-th entry V[i,j] represents agent i's the value for item j
+        A: an allocation; a binary n x m 2D numpy array consisting of 0s and 1s
+        chores: True / False indicating whether the input V represents a chores instance (True) or goods instance (False)
+    Output:
+        True / False: return True if A is envy-free-up-to-one-item (EF1) w.r.t. the input valuation profile V
+    """
     (n, m) = np.shape(V)
     for j1 in range(n):
         for j2 in range(n):
@@ -194,6 +262,15 @@ def isef1(V, A, chores=False):
 
 
 def isefx(V, A, chores=False):
+    """
+    Input:
+        V: a valuation profile; an n x m 2D numpy array consisting of integers whose i.j-th entry V[i,j] represents agent i's the value for item j
+        A: an allocation; a binary n x m 2D numpy array consisting of 0s and 1s
+        chores: True / False indicating whether the input V represents a chores instance (True) or goods instance (False)
+    Output:
+        True / False: return True if A is envy-free-up-to-any-item (EFX) w.r.t. the input valuation profile V
+    """
+
     (n, m) = np.shape(V)
     for j1 in range(n):
         for j2 in range(n):
@@ -228,18 +305,30 @@ def isefx(V, A, chores=False):
 
 
 def ispo(V, A, chores=False, tol=10**-5):
+    """
+    Input:
+        V: a valuation profile; an n x m 2D numpy array consisting of integers whose i.j-th entry V[i,j] represents agent i's the value for item j
+        A: an allocation; a binary n x m 2D numpy array consisting of 0s and 1s
+        chores: True / False indicating whether the input V represents a chores instance (True) or goods instance (False)
+    Output:
+        True / False: return True if A satisfies Pareto optimality (PO) w.r.t. the input valuation profile V
+    Notes:
+        This procedure works by solving an ILP to find an allocation B with maximum possible total utilitarian welfare 
+        such that every agent has at least the same utility in B as they do in A,
+        and then checking whether B Pareto dominates A by by checking if there is an agent who has strictly greater utility in B.
+        Notice that the ILP should always have a feasible solution, since A is a feasible solution.
+
+    """
     (n, m) = np.shape(V)
+    assert(A.shape == (n,m))
+    assert(np.all(np.sum(A, axis=0) == 1))
     U = np.zeros(n)
     for j in range(n):
         vj = V[j, :]
         aj = A[j, :]
-        U[j] = int(np.dot(vj, aj))
+        U[j] = np.dot(vj, aj)
+    U = U.astype(int)
     Vmax = [np.sum(V[j, :]) for j in range(n)]
-    """
-    print('V\n', V)
-    print('A\n', A)
-    print('U ', U)
-    """
 
     model = Model('po')
     model.setParam('OutputFlag', 0)
@@ -251,102 +340,19 @@ def ispo(V, A, chores=False, tol=10**-5):
         for i in range(m):
             xvars[j, i] = model.addVar(lb=0, ub=1, vtype=GRB.BINARY, name='x_{}_{}'.format(j, i))
 
-    # add variables vbvars for a-gents' values for allocation B
+    # add variables vbvars for agents' values for allocation B
+    # notice that the lower and upper bounds on the value of vbvars are constraints ensuring that 
+    # every agent j values its bundle in B at least as much as it values its bundle in A
+    # in a goods instance:
+    #   the lower bound is the agent's value (utility) for its bundle in A
+    #   the upper bound is the value of all goods
+    # in a chores instance:
+    #   the lower bound is the agent's value (disutility) for its bundle in A
+    #   the upper bound is 0, i.e., its value for receiving zero chores
     vbvars = dict()
     for j in range(n):
-        vbvars[j] = model.addVar(lb=U[j], ub=int(chores)*np.sum(V[j, :]), vtype=GRB.INTEGER, name='vb_{}'.format(j))
-
-    # add variables yvars to check for strict improvements
-    yvars = dict()
-    for j in range(n):
-        yvars[j] = model.addVar(lb=0, ub=1, vtype=GRB.BINARY, name='y_{}'.format(j))
-
-    # add variable to count the number of agents who strictly improve
-    zvar = model.addVar(lb=0, ub=n, vtype=GRB.INTEGER, name='z')
-
-    # add allocation constraints
-    for i in range(m):
-        model.addConstr(1 == quicksum([xvars[j, i] for j in range(n)]))
-
-    # add value constraints
-    for j in range(n):
-        model.addConstr(vbvars[j] == quicksum([V[j, i] * xvars[j, i] for i in range(m)]))
-
-    # add improvement constraints
-    for j in range(n):
-        model.addConstr(yvars[j] >= (vbvars[j] - U[j])/np.sum(V[j, :]))
-        model.addConstr(yvars[j] <= vbvars[j] - U[j])
-
-    # add Pareto dominance constrain
-    model.addConstr(zvar == quicksum([yvars[j] for j in range(n)]))
-
-    model.setObjective(zvar, GRB.MAXIMIZE)
-
-    model.optimize()
-
-    status = model.Status
-
-    if int(status) == int(GRB.INFEASIBLE):
-        if tol < 0.1:
-            model.reset()
-            logging.debug('ILP infeasible, retry with tolerance {}'.format(tol * 10))
-            return ispo(V, A, tol=tol*10)
-        logging.debug('ILP infeasible with tolerance {}'.format(tol))
-
-    print('status ', model.Status)
-    B = np.zeros((n, m))
-    for j in range(n):
-        for i in range(m):
-            B[j, i] = xvars[j, i].X
-    UB = np.zeros(n)
-    for j in range(n):
-        UB[j] = np.dot(B[j, :], V[j, :])
-    print('# improvements ', zvar.X)
-    # print('improved allocation:\n', B)
-    print('utilities under original allocation: ', U)
-    print('utilities under improved allocation: ', UB)
-    print('improvements: ', UB - U)
-
-    if int(status) == int(GRB.OPTIMAL):
-        if zvar.X == 0:
-            return True
-        else:
-            if np.any(UB - U > 0.1):
-                return False
-            else:
-                logging.debug('more numerical issues, claimed improvements {}, actual improvements {}'.format(zvar.X, UB-U))
-                return True
-    return False
-
-
-def ispo2(V, A, chores=False, tol=10**-5):
-    (n, m) = np.shape(V)
-    U = np.zeros(n)
-    for j in range(n):
-        vj = V[j, :]
-        aj = A[j, :]
-        U[j] = int(np.dot(vj, aj))
-    Vmax = [np.sum(V[j, :]) for j in range(n)]
-    """
-    print('V\n', V)
-    print('A\n', A)
-    print('U ', U)
-    """
-
-    model = Model('po')
-    model.setParam('OutputFlag', 0)
-    model.setParam('IntFeasTol', tol)
-
-    # add variables xvars for the allocation
-    xvars = dict()
-    for j in range(n):
-        for i in range(m):
-            xvars[j, i] = model.addVar(lb=0, ub=1, vtype=GRB.BINARY, name='x_{}_{}'.format(j, i))
-
-    # add variables vbvars for a-gents' values for allocation B
-    vbvars = dict()
-    for j in range(n):
-        vbvars[j] = model.addVar(lb=U[j], ub=int(chores)*np.sum(V[j, :]), vtype=GRB.CONTINUOUS, name='vb_{}'.format(j))
+        vbvars[j] = model.addVar(lb=U[j], ub=int(1 - chores)*np.sum(V[j, :]), vtype=GRB.INTEGER, name='vb_{}'.format(j))
+    print(j, U[j], int(chores)*np.sum(V[j, :]))
 
     # add allocation constraints
     for i in range(m):
@@ -360,8 +366,8 @@ def ispo2(V, A, chores=False, tol=10**-5):
     model.setObjective(quicksum([vbvars[j] for j in range(n)]), GRB.MAXIMIZE)
 
     model.optimize()
-
     status = model.Status
+    print(status)
 
     if int(status) == int(GRB.INFEASIBLE):
         if tol < 0.1:
@@ -378,7 +384,7 @@ def ispo2(V, A, chores=False, tol=10**-5):
     UB = np.zeros(n)
     for j in range(n):
         UB[j] = np.dot(B[j, :], V[j, :])
-    """
+    """ Diagnostics
     print('improved allocation:\n', B)
     print('utilities under improved allocation: ', UB)
     print('utilities under original allocation: ', U)
